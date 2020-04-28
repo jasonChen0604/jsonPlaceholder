7ab037262bb06dde649fea8e6dcdb1b131b0dcfd
Lily <jason.chen.test123@gmail.com>
2020-04-28 21:08:46 +0800

    [A] photo layout & item_photo_list

--- app/src/main/java/com/example/json/PhotoActivity.kt
+++ app/src/main/java/com/example/json/PhotoActivity.kt
@@ -2,11 +2,28 @@ package com.example.json
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.json.JSONArray
 
 class PhotoActivity : AppCompatActivity() {
+    var TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
+
+      //  getData(this).execute()
+        //rclPhoto.layoutManager = GridLayoutManager(this, 3)
+        //rclPhoto.adapter = PhotoAdapter(    )
+    }
+
+    private fun getData() {
+        val data = "https://jsonplaceholder.typicode.com/photos"
+
+        //rclPhoto.layoutManager = GridLayoutManager(this, 4)
+
+        //rclPhoto.adapter = PhotoAdapter()
+
+
     }
 }
+
